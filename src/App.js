cc867ecfb1e8e0b51ad49ba1af13a05cb0b57108
Shamith Rosa <shamithrosa@outlook.com>
2022-06-19 01:10:54 +0530

    misc changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 
 function App() {
+  const [data, getData] = useState([]);
+  const URL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((response) => {
+        getData(response);
+      });
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submit btn clicked");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello Dr. Ruth</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 id="header3">Sample Sheet - {new Date().toLocaleTimeString()}</h3>
+        <Form onSubmit={handleSubmit}>
+          <Button type="submit">Click Me</Button>
+        </Form>
+
+        <hr></hr>
+        <Table bordered="true">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>User Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, i) => (
+              <tr key={i}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </header>
     </div>
   );
